76437447f4254e37bd4adc2f2290ffb8ae0c8ca8
yoyzhang <alexuzhang@gmail.com>
2022-06-19 15:50:06 +0800

    feat: setup uno css

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -7,5 +7,6 @@ const uppercase = computed(( ) => props.name.toLocaleUpperCase())
 </script>
 
 <template>
-  <div>hello222 {{uppercase}}</div>
+  <div text="red" font-sans>hello222, {{uppercase}}</div>
+  <div i-carbon-bee text-yellow></div>
 </template>
